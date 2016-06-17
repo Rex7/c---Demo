a63b77296f57e21c051be1a977f5718ffb560c18
Rex7 <regischarles7@gmail.com>
2016-06-17 15:26:16 +0530

    updated program

---
+++ StringDemo.cpp
@@ -0,0 +1,30 @@
+#include <iostream.h>
+#include <string>
+int main()
+{
+	std::string one,two,s,word,substring;
+	std::cout<<"\n Enter Name 1:";
+	std::getline(std::cin,one);
+	std::cout<<"\n Enter String 2:";
+	std::getline(std::cin,two);
+int size=one.length();
+std::cout<<"\nlength of string one:"<<size;
+std::cout<<"\nComparing string one and two";
+if((one.compare(two))==0)
+{
+	std::cout<<"\nBoth String Are Equal";
+}
+else
+	std::cout<<"\nIts Not Equal";
+	//enter to find a word from a given sentence
+	std::cout<<"\nEnter a word to find?";
+	std::getline(std::cin,word);
+	//finding the index of the given word
+	int index=one.find(word);
+	std::cout<<"index of the word :"<<index;
+	//with the obove information separating the string from base string
+	substring=one.substr(index,size-index);
+	std::cout<<"\n SubString is :"<<substring;
+	
+	
+}

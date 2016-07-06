97b26262c39d2081bd502ee1c71cf7ce3bfba98a
Rex7 <regischarles7@gmail.com>
2016-07-06 14:41:33 +0530

    operator overloading

--- operatoroverloading.cpp
+++ operatoroverloading.cpp
@@ -21,6 +21,7 @@ void display_marks()
 {
 	std::cout<<"\nMarks in English:"<<english<<"\n Marks in Math:"<<math<<"\nMarks in Science:"<<science;
 }
+//overloading + operator
 Marks operator+(Marks m)
 {
 	Marks ne;
@@ -29,10 +30,23 @@ Marks operator+(Marks m)
 	ne.science=this->science+m.science;
 	return ne;
 }
+Marks operator-(Marks m);
 };
+Marks Marks::operator-(Marks m)
+{
+		Marks ne;
+	ne.english=this->english-m.english;
+	ne.math=this->math-m.math;
+	ne.science=this->science-m.science;
+	return ne;
+}
 int main()
 {
 Marks m(55,65,45),m2(65,85,25),m3;
 m3=m+m2;
-m3.display_marks();	
+m3.display_marks();
+m3=m-m2;
+std::cout<<"\nperforming minus operation on marks:\n";
+m3.display_marks();
+return 0;	
 }
